491637ed680bd76a9c119c8f7992a87af9459106
limits1214 <syl@SYL35EC>
2026-05-02 16:30:03 +0900

    chore: FastNoiseLite 지형 노이즈 의존성 추가

--- Engine/Private/Chunk.cpp
+++ Engine/Private/Chunk.cpp
@@ -1,8 +1,11 @@
 #include "Chunk.h"
+#include <FastNoiseLite.h>
 
 NS_USING(Engine)
 
 std::vector<QUAD> CChunk::GenerateQuad()
 {
+    FastNoiseLite noise;
+    noise.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2);
     return std::vector<QUAD>();
 }
